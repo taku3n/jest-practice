e40efb07ad7f7f622b42405b80e10fbff8bcde8f
takumi <hiatana72@gmail.com>
2022-06-12 15:37:31 +0900

    split function into I/O units

--- app.js
+++ app.js
@@ -1,22 +1,16 @@
+// toFizzBuzz()の結果をコンソールログに書き出す
 const app = () => {
   for (let i = 1; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-      console.log("FizzBuzz");
-      continue;
-    }
-
-    if (i % 3 == 0) {
-      console.log("Fizz");
-      continue;
-    }
-
-    if (i % 5 == 0) {
-      console.log("Buzz");
-      continue;
-    }
-
-    console.log(i);
+    console.log(toFizzBuzz(i));
   }
+};
+
+// 引数の数字に対しFizzBuzz判定をする
+const toFizzBuzz = (num) => {
+  if (num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
+  if (num % 3 == 0) return "Fizz";
+  if (num % 5 == 0) return "Buzz";
+  return num;
 };
 
 module.exports = app;
