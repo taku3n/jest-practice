46b3aaedfca8ea1a0746c6f55250c138f0289478
takumi <hiatana72@gmail.com>
2022-06-13 23:20:38 +0900

    add function to compare datetime

--- app.js
+++ app.js
@@ -7,4 +7,19 @@ const app = () => {
   }
 };
 
+// 引数のDatetimeを比較しどちらが先かの判定結果をコンソールログに書き出す
+const compareDatetime = () => {
+  var date1 = new Date("2022/06/13");
+  var date2 = new Date("2022/06/11");
+
+  if (date1.getTime() === date2.getTime()) {
+    console.log("同じ日付です");
+  } else if (date1 > date2) {
+    console.log("date1の方が先の日付です");
+  } else if (date1 < date2) {
+    console.log("date2の方が先の日付です");
+  }
+};
+
 module.exports = app;
+module.exports = compareDatetime;
